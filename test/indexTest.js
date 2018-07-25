3d1675902195de1fb21fe729a0bca6d45e61e596
Danny Garcia <danielgarcia1812@gmail.com>
2018-07-25 17:39:28 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,7 @@
 const expect = chai.expect;
 
 describe('index.js', function() {
-  describe('distanceFromHqInBlocks(2500)', function() {
+  describe('distanceFromHqInBlocks()', function() {
     it('returns a distance in blocks', function() {
       expect(distanceFromHqInBlocks(43)).to.equal(1);
     });
